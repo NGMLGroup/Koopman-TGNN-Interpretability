58c427943620865086921324de4ce1ced617c765
MicheleUIT <mgu111@uit.no>
2024-04-05 16:22:00 +0200

    + K classification sweep

--- train_K_classifier.py
+++ train_K_classifier.py
@@ -3,10 +3,16 @@ import wandb
 import os
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
-from torch_geometric.datasets import TUDataset
-from tqdm import trange
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from einops import rearrange
+
+from dataset.utils import process_FB
+from DMD.dmd import KANN
 
 seed = 42
 random.seed(seed)
@@ -14,12 +20,18 @@ torch.manual_seed(seed)
 np.random.seed(seed)
 
 config = {
-    'lr': 0.08292410152754211,
-    'n_layers': 3,
-    'activation': 'tanh',
-    'h_dim': 32,
-    'epochs': 100
-}
+        'reservoir_size': 16,
+        'input_scaling': 1,
+        'reservoir_layers': 5,
+        'leaking_rate': 0.1,
+        'spectral_radius': 1,
+        'density': 1,
+        'reservoir_activation': 'tanh',
+        'alpha_decay': False,
+        'add_self_loops': False,
+        'b_leaking_rate': True,
+        'dim_red': 16
+        }
 
 wandb.init(project="koopman", config=config)
 config = wandb.config
@@ -33,47 +45,90 @@ else:
     device = torch.device('cpu')
 
 
-dataset = TUDataset(root='/ESGNN/data/MUTAG', name='MUTAG')
+# Train ESN readout
 
-koops = torch.load('KOP/quadr_TruncatedSVD_5.pt')
+# Load Facebook dataset and process it with DynGESN
+dataset, states = process_FB(config, device, ignore_file=True, verbose=False)
 
-K_train, K_test, y_train, y_test = train_test_split(koops, dataset.y, test_size=0.3, random_state=seed)
+# Train readout on the last time step
+inputs, test_inputs, labels, test_labels = train_test_split(dataset.inputs, dataset.targets, test_size=0.2, random_state=seed)
 
-modules = []
-if config.activation == 'relu':
-    activation = torch.nn.ReLU()
-elif config.activation == 'tanh':
-    activation = torch.nn.Tanh()
+classifier = LogisticRegression(max_iter=5000, random_state=seed) # Create the classifier
+classifier.fit(inputs, labels) # Train the classifier
+y_pred = classifier.predict(test_inputs) # Test the classifier
+accuracy = accuracy_score(test_labels, y_pred) # Compute the accuracy
+wandb.log({'readout_acc': accuracy})
 
-for i in range(config.n_layers):
-    in_dim = koops.shape[1] if i == 0 else config.h_dim
-    out_dim = 2 if i == config.n_layers-1 else config.h_dim
-    modules.append(torch.nn.Linear(in_dim, out_dim))
-    if i != config.n_layers-1:
-        modules.append(activation)
 
-classifier = torch.nn.Sequential(*modules).to(device)
+# Train a classifier on the Koopman operators
 
-optimizer = torch.optim.Adam(classifier.parameters(), lr=config.lr)
-loss_fn = torch.nn.CrossEntropyLoss()
+# Load full reservoir states (with time dimension)
+X_train, X_test, labels, test_labels = train_test_split(states.inputs, states.targets, test_size=0.3, random_state=seed)
 
-epochs = config.epochs
-classifier.train()
-losses, loss_e = [], []
+# Dim reduction
+dim_red = config.dim_red
+pca = PCA(n_components=dim_red)
+X_train_red = pca.fit_transform(X_train.reshape(-1, X_train.shape[-1]))
+X_test_red = pca.transform(X_test.reshape(-1, X_test.shape[-1]))
+X_train_red = rearrange(X_train_red, '(b t) f -> b t f', b = X_train.shape[0], t=X_train.shape[1], f=dim_red)
+X_test_red = rearrange(X_test_red, '(b t) f -> b t f', b = X_test.shape[0], t=X_test.shape[1], f=dim_red)
 
-for epoch in trange(epochs):
-    for k, y in zip(K_train, y_train):
-        out = classifier(k)
-        optimizer.zero_grad()
-        loss = loss_fn(out, y.type(torch.LongTensor).squeeze().to(device))
-        wandb.log({'loss': loss, 'epoch': epoch})
-        loss.backward()
-        optimizer.step()
-        loss_e.append(loss.clone().detach())
-    losses.append(torch.tensor(loss_e).mean())
+# Compute a per sample Koopman operator
+Ks = []
+for x in X_train_red:
+    kann_ = KANN(x[np.newaxis,:,:], k=0, emb=None)
+    KL = kann_.compute_KOP()
+    Ks.append(KL.flatten())
+Ks = np.stack(Ks, axis=0)
 
-classifier.eval()
-test_acc = (classifier(K_test).cpu().argmax(dim=-1) == y_test).sum()/len(y_test)
-print(f"Test accuracy: {test_acc}")
+# Train the classifier
+Kclassifier = LogisticRegression(max_iter=1000)
+Kclassifier.fit(Ks, labels)
 
-wandb.log({'test_acc': test_acc})
+# Test the classifier
+Ks_te = []
+for x in X_test_red:
+    kann_ = KANN(x[np.newaxis,:,:], k=0, emb=None)
+    KL = kann_.compute_KOP()
+    Ks_te.append(KL.flatten())
+Ks_te = np.stack(Ks_te, axis=0)
+y_pred = Kclassifier.predict(Ks_te)
+accuracy = accuracy_score(test_labels, y_pred)
+wandb.log({'K_acc': accuracy})
+
+
+# Plots
+
+# Plot covariance matrix of reduced states
+fig, ax = plt.subplots()
+cov = ax.imshow(X_train_red.reshape(-1,X_train_red.shape[-1]).T @ X_train_red.reshape(-1,X_train_red.shape[-1]), cmap='viridis')
+plt.colorbar(cov, ax=ax)
+wandb.log({"cov_img": wandb.Image(fig)})
+plt.close(fig)
+
+
+# Plot state distribution of the first 2 PCA components
+idx0 = labels == 0
+idx1 = labels == 1
+label_0 = X_train_red[idx0, -1, :2]
+label_1 = X_train_red[idx1, -1, :2]
+
+# Create a figure with two subplots
+fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+# Plot the first histogram in the first subplot
+hist0 = axs[0].hist2d(label_0[:, 0], label_0[:, 1], bins=10, cmap='Blues', alpha=0.6)
+axs[0].set_xlabel('PC 0')
+axs[0].set_ylabel('PC 1')
+axs[0].set_title('2D Histogram - Label 0')
+plt.colorbar(hist0[3], ax=axs[0])
+# Plot the second histogram in the second subplot
+hist1 = axs[1].hist2d(label_1[:, 0], label_1[:, 1], bins=10, cmap='Reds', alpha=0.6)
+axs[1].set_xlabel('PC 0')
+axs[1].set_ylabel('PC 1')
+axs[1].set_title('2D Histogram - Label 1')
+plt.colorbar(hist1[3], ax=axs[1])
+# Adjust the spacing between subplots
+plt.tight_layout()
+
+wandb.log({"hist_PC_img": wandb.Image(fig)})
+plt.close(fig)
